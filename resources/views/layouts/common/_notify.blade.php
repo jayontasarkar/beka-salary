2a9286134029d83456b4add2662e37e429a6242e
Jayonta Sarkar <jayonta.aiub@gmail.com>
2017-10-25 13:45:11 +0600

    Template Mastering Completed

---
+++ resources/views/layouts/common/_notify.blade.php
@@ -0,0 +1,18 @@
+<script type="text/javascript">
+	$(document).ready(function() {
+		$.notify({
+            icon: " {{ isset($icon) ? $icon : 'pe-7s-gift' }}",
+            title: "{{ isset($title) ? $title : 'Bootstrap notify' }}",
+            message: "{{ isset($message) ? $message : 'Turning standard Bootstrap alerts into notify like notifications' }}"
+        },{
+            type: 'info',
+            timer: 4000,
+            // can be success, info, primary, warning, danger
+            type: "{{ isset($type) ? $type : 'info' }}",
+            placement: {
+                from: "{{ isset($from) ? $from : 'bottom' }}",
+                align: "{{ isset($align) ? $align : 'right' }}"
+            }
+        });
+	});
+</script>
